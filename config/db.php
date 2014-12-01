eea388c0bf12e1bd17abc1b370f612230a13a8c5
constantine <constantine.chik@gmail.com>
2014-12-01 19:30:11 +0200

    Clear project

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=constantinemarket',
+    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '1',
+    'password' => '',
     'charset' => 'utf8',
 ];
