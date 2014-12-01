f9eca725840c5af0d94eb8822ba496813c739ea1
constantine <constantine.chik@gmail.com>
2014-12-01 18:37:33 +0200

    Initial commit

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=constantinemarket',
+    'username' => 'root',
+    'password' => '1',
+    'charset' => 'utf8',
+];
